1d210df55f1b4dc514d2d8e6d3997de1b43bb3d1
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2023-09-03 23:55:21 +0530

    Update pascalsTriangle.java

--- ArraysOne/pascalsTriangle.java
+++ ArraysOne/pascalsTriangle.java
@@ -1,1 +1,17 @@
-//
+public class PascalsTriangle {
+
+    public static int nCr(int r, int c){
+        int res=1;
+        for(int i=0;i<c-1;i++)
+        {
+            res = res * ((r-1) - i);
+            res /= i+1;
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int r=5;
+        int c=3;
+        System.out.println("The answer is::"+nCr(6,4));
+    }
+}
