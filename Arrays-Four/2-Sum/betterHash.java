2af483a174f94cdaa41c86625dcc89535f0ca25c
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2023-09-17 00:45:48 +0530

    Update betterHash.java

--- Arrays-Four/2-Sum/betterHash.java
+++ Arrays-Four/2-Sum/betterHash.java
@@ -1,1 +1,17 @@
-//
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int a[] = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+           int more = target - nums[i];
+           if(map.containsKey(more)){
+               a[0] = map.get(more);
+               a[1] = i;
+               return a;
+           }
+           map.put(nums[i], i);
+        }
+        return a;
+    }
+}
