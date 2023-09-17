1a2231eb4fc9c10702717af4f976b3415f8f23b6
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2023-09-17 23:10:41 +0530

    Create bruteForce.java

---
+++ Arrays-Four/4-Sum/bruteForce.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+       int n = nums.length; // size of the array
+        Set<List<Integer>> set = new HashSet<>();
+
+        // checking all possible quadruplets:
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    for (int l = k + 1; l < n; l++) {
+                        // taking bigger data type
+                        // to avoid integer overflow:
+                        long sum = (long)nums[i] + nums[j];
+                        sum += nums[k];
+                        sum += nums[l];
+
+                        if (sum == target) {
+                            List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
+                            Collections.sort(temp);
+                            set.add(temp);
+                        }
+                    }
+                }
+            }
+        }
+        List<List<Integer>> ans = new ArrayList<>(set);
+        return ans;
+    }
+}
