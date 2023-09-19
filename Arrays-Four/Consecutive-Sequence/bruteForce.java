f1cd2ed03f8c3a65056e68cfa9c23e2a1fc508df
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2023-09-20 00:44:46 +0530

    Update bruteForce.java

--- Arrays-Four/Consecutive-Sequence/bruteForce.java
+++ Arrays-Four/Consecutive-Sequence/bruteForce.java
@@ -1,1 +1,27 @@
-//
+class Solution {
+    public static boolean linearSearch(int[] nums, int el)
+    {
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i] == el)
+            return true;
+        }
+        return false;
+    }
+    public int longestConsecutive(int[] nums) {
+        
+        int count = 0, max = 0;
+        for(int i=0;i<nums.length;i++){
+            int x = nums[i];
+            count = 0;
+            while(linearSearch(nums, x+1)){
+                count++; x++;
+            }
+            if(count>max)
+            max = count;
+        }
+        if(nums.length == 0)
+        return 0;
+        return max+1;
+    }
+}
