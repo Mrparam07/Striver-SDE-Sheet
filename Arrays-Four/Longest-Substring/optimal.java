c6341ba9e48fa5d3d32941b3b5dfcf10446f9928
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2023-09-23 00:10:10 +0530

    Update optimal.java

--- Arrays-Four/Longest-Substring/optimal.java
+++ Arrays-Four/Longest-Substring/optimal.java
@@ -1,1 +1,23 @@
-//
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length() == 0)
+        return 0;
+         int max = 0;
+         int l = 0;
+         Set<Character> set = new HashSet<>();
+        
+        for(int r = 0;r<s.length();r++)
+        { 
+            if(set.contains(s.charAt(r))){
+
+                while(l<r && set.contains(s.charAt(r))){
+                    set.remove(s.charAt(l));
+                    l++;
+                }
+            }
+            set.add(s.charAt(r));
+            max = Math.max(max, r-l+1);
+        }
+        return max;
+    }
+}
