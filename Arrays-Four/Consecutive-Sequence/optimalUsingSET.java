16759c6f4b8474198ffa9304c99702973f5e6048
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2023-09-28 23:07:46 +0530

    Update optimalUsingSET.java

--- Arrays-Four/Consecutive-Sequence/optimalUsingSET.java
+++ Arrays-Four/Consecutive-Sequence/optimalUsingSET.java
@@ -1,28 +1,25 @@
 class Solution {
+
     public int longestConsecutive(int[] nums) {
-        int n = nums.length;
-        if(n == 0)
-        return 0;
-    
-        int longest = 1;
+        int count = 0;
+        int max = 0;
+        
         Set<Integer> set = new HashSet<>();
-        for(int i=0;i<n;i++)
-        {
+        for(int i=0;i<nums.length;i++)
             set.add(nums[i]);
-        }
-        for (int it : set) {
-            // if 'it' is a starting number
-            if (!set.contains(it - 1)) {
-                // find consecutive numbers
-                int cnt = 1;
-                int x = it;
-                while (set.contains(x + 1)) {
-                    x = x + 1;
-                    cnt = cnt + 1;
-                }
-                longest = Math.max(longest, cnt);
+
+        for(int i: set)
+        {
+            if(!set.contains(i-1)){
+                count = 0;
+                int k = i;
+                    while(set.contains(k)){
+                    count++;
+                    k++;
             }
+            max = Math.max(max, count);
+            } 
         }
-        return longest;
+        return max;
     }
 }
