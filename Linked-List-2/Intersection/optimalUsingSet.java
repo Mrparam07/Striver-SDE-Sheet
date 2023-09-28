4ae2446252b91be5aaec2adea02bb8a858ed7dd4
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2023-09-28 23:58:17 +0530

    Create optimalUsingSet.java

---
+++ Linked-List-2/Intersection/optimalUsingSet.java
@@ -0,0 +1,35 @@
+// TC::O(m+n) SC::O(1)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+       
+       ListNode l1 = headA;
+       ListNode l2 = headB;
+       
+        Set<ListNode> set = new HashSet<>();
+        while(l1 !=null){
+            set.add(l1);
+            l1 = l1.next;
+        }
+            
+
+        while(l2 != null){
+            if(set.contains(l2))
+            return l2;
+            
+            l2 = l2.next;
+        }
+
+       return null;
+    }
+}
