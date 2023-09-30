2861063a7c1a6019100a8e1fb915b28fbf0b18ad
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2023-09-30 23:21:04 +0530

    Update bruteForce.java

--- Kadane's/bruteForce.java
+++ Kadane's/bruteForce.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxSubArray(int[] nums) {
         int sum;
         int max = Integer.MIN_VALUE;
-
+ 
         for(int i=0;i<nums.length;i++)
         {
            sum = 0;
