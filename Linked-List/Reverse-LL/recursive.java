8c73ba54ae10391cb9826f604ca9b10b0939e181
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2023-10-04 23:31:07 +0530

    Update recursive.java

--- Linked-List/Reverse-LL/recursive.java
+++ Linked-List/Reverse-LL/recursive.java
@@ -2,7 +2,7 @@ class Solution {
     public ListNode reverseList(ListNode head) {
         if(head == null || head.next == null)
         return head;
-        ListNode ans = reverseList(head.next);
+        ListNode ans = reverseList(head.next); 
         head.next.next = head;
         head.next = null;
         
