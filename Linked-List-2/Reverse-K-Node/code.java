a2c5ae1ce9a6651aa721b11c9cd1bb48949ba1a7
Parambir Kumar <114753610+Mrparam07@users.noreply.github.com>
2023-10-09 00:07:47 +0530

    Update code.java

--- Linked-List-2/Reverse-K-Node/code.java
+++ Linked-List-2/Reverse-K-Node/code.java
@@ -1,1 +1,43 @@
-//
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        int count = 0;
+        ListNode temp = head;
+
+        while(temp != null){
+            count++; 
+            temp = temp.next;
+        }
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode cur;
+        ListNode nex;
+
+        while(count>=k)
+        {
+            cur = pre.next;
+            nex = cur.next;
+            for(int i=1;i<k;i++)
+            {
+                cur.next = nex.next;
+                nex.next = pre.next;
+                pre.next = nex;
+                nex = cur.next;
+            }
+            pre = cur;
+            count = count - k;
+        }
+        return dummy.next;
+    }
+}
